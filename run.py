12ee3e37ac07854ae41024e876a978e0ad130aef
Varun Ved <varunmved@gmail.com>
2015-11-23 15:40:41 -0800

    THIS IS LIKE A MINI HACKAHTONNNNN

--- run.py
+++ run.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import twilio.twiml
 import os
-
+ 
 app = Flask(__name__)
+ 
+# Try adding your own number to this list!
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+    "+19167516308": "Varun"
+}
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming requests."""
+    # Get the caller's phone number from the incoming Twilio request
+    from_number = request.values.get('From', None)
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+ 
+    # if the caller is someone we know:
+    if from_number in callers:
+        # Greet the caller by name
+        resp.say("Hello " + callers[from_number])
+    else:
+        resp.say("Hello Monkey")
  
     return str(resp)
  
