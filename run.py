2b2790aa233835973ed655a9ee8a6366d77c4ee1
Varun Ved <varunmved@gmail.com>
2015-11-25 15:15:55 -0800

    maybe i need to fix that

--- run.py
+++ run.py
@@ -19,6 +19,46 @@ varun_number_twilio = "+19164762325"
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        caller = callers[from_number]
+    else:
+        caller = "Monkey"
+ 
+    resp = twilio.twiml.Response()
+    # Greet the caller by name
+    resp.say("Hello " + caller)
+     
+    # Say a command, and listen for the caller to press a key. When they press
+    # a key, redirect them to /handle-key.
+    with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
+        g.say("To play fizz buzz, press 1. Press any other key to start over.")
+ 
+    return str(resp)
+ 
+@app.route("/handle-key", methods=['GET', 'POST'])
+def handle_key():
+    """Handle key press from a user."""
+ 
+    # Get the digit pressed by the user
+    digit_pressed = request.values.get('Digits', None)
+    if digit_pressed == "1":
+        resp = twilio.twiml.Response()
+        # Dial (310) 555-1212 - connect that number to the incoming caller.
+        #resp.dial("+13105551212")
+        # If the dial fails:
+        resp.say("The call failed, or the remote party hung up. Goodbye.")
+
+ 
+        return str(resp)
+ 
+    # If the caller pressed anything but 1, redirect them to the homepage.
+    else:
+        return redirect("/")
+ 
+'''
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
     # Get the caller's phone number from the incoming Twilio request
     from_number = request.values.get('From', None)
     resp = twilio.twiml.Response()
@@ -40,7 +80,6 @@ def hello_monkey():
 def handle_key():
     """Handle key press from a user."""
  
-    '''
     # Get the digit pressed by the user
     digit_pressed = request.values.get('Digits', None)
     if digit_pressed == "*":
@@ -54,13 +93,14 @@ def handle_key():
     # If the caller pressed anything but 1, redirect them to the homepage.
     else:
         return redirect("/")
-    '''
+   
     num_in = request.values.get('Digits',None)
     out = fizzbuzz(num_in)
     resp = twilio.twiml.Response()
     resp.say(out)
     return redirect("/")
-
+  
+'''
 def fizzbuzz(digit_in):
     output_str = ""
     for i in range (0,digit_in):
