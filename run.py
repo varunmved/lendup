ebc66c76cef055e7cca269ddc0c66f5f0cd0e208
Varun Ved <varunmved@gmail.com>
2015-12-02 09:33:34 -0800

    fix

--- run.py
+++ run.py
@@ -6,10 +6,12 @@ from wtforms import TextField
 from flask import jsonify
 from wtforms.validators import Required, Length, ValidationError
 import os
-import re
- 
+
 app = Flask(__name__)
- 
+
+'''
+consts
+'''
 # Try adding your own number to this list!
 callers = {
     "+14158675309": "Curious George",
@@ -17,12 +19,12 @@ callers = {
     "+14158675311": "Virgil",
     "+19167516308": "Varun",
     "+19164762325": "VarunTwilio"
-
-
 }
-url = "https://tranquil-savannah-8709.herokuapp.com"
+
+urlHeroku = "https://tranquil-savannah-8709.herokuapp.com"
 varun_number_twilio = "+19164762325"
 varun_number_verizon = "+19167516308"
+
 '''
 utils
 '''
@@ -33,18 +35,17 @@ def call(numIn):
     account_sid = "AC1cc3d40a4dca1cd0ff1af031ff1b14ca"
     auth_token = "5011a96781ef26d63904ca3b8e3ccb35"
     client = TwilioRestClient(account_sid, auth_token)
- 
-    # Make the call
-    call = client.calls.create(to=varun_number_twilio,  # Any phone number
-                           from_=varun_number_verizon, # Must be a valid Twilio number
-                           url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
-    print call.sid
 
+    # Make the call
+    call = client.calls.create(to=numIn,  # Any phone number
+                           from_=varun_number_twilio, # Must be a valid Twilio number
+                           url=urlHeroku)
+    print call.sid
     return "called"
 
-def fizzbuzz(digit_in):
+def fizzbuzz(fizzBuzzCalcValue):
     output_str = ""
-    for i in range (0,digit_in):
+    for i in range (0,fizzBuzzCalcValue):
         if i % 3 == 0 and i % 5 == 0:
             output_str+=(' Fizz Buzz ')
         elif i % 3 ==0:
@@ -56,28 +57,27 @@ def fizzbuzz(digit_in):
     return output_str
 
 
-def startsWithPlus(form, phone_num):
-  p = re.compile("^\+")
-  if p.match(phone_num.data) == None:
-    raise ValidationError('Phone number should start with a plus.')
-
-def containsDigits(form, phone_num):
-  p = re.compile('\+?\d{1,15}')
-  if p.match(phone_num.data) == None:
-    raise ValidationError('Phone number should only contain upto 15 digits.')
-
+def isValid(form, phoneNumber):
+  rawTextField = str(phoneNumber)
+  valueDesiredString = "value="
+  valueIndex = rawTextField.index(valueDesiredString) +6
+  numStr = rawTextField[valueIndex:len(rawTextField)-1]
+  numStr = (numStr[1:13])
+  print(numStr)
+  print(len(numStr))
+  if len(numStr)!= 12 or numStr[0] != "+" or not str.isdigit(numStr[1:13]):
+      raise ValidationError("Your phone number isn't a valid US/Canada number")
 
 '''
 classes
 '''
 class MyForm(Form):
-  call_failed = False
-  phone_num = TextField('phone_num', validators=[Required(), Length(min=1, max=16),startsWithPlus, containsDigits])
+  failedCall = False
+  phoneNumber = TextField('phoneNumber', validators=[Required(), Length(min=12, max=12),isValid])
 
 '''
-routes
+twilio routes
 '''
-
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     from_number = request.values.get('From', None)
@@ -85,36 +85,31 @@ def hello_monkey():
         caller = callers[from_number]
     else:
         caller = "Monkey"
- 
+
     resp = twilio.twiml.Response()
     # Greet the caller by name
     resp.say("Hello " + caller)
-     
+
     # Say a command, and listen for the caller to press a key. When they press
     # a key, redirect them to /handle-key.
     with resp.gather(numDigits=2, action="/handle-key", method="POST") as g:
         g.say("To play fizz buzz, please enter a 2 digit number")
- 
+
     return str(resp)
     #return render_template('template.html')
- 
+
 @app.route("/handle-key/", methods=['GET', 'POST'])
 def handle_key():
     """Handle key press from a user."""
- 
+
     # Get the digit pressed by the user
     digit_pressed = request.values.get('Digits', None)
     if digit_pressed.isdigit():
         rawVal = int(digit_pressed)
         resp = twilio.twiml.Response()
-        '''
-        with resp.gather(numDigits=3, action="/handle-fizzbuzz", method="POST", finishOnKey= "*") as g:
-            hello  = fizzbuzz(int(g))
-        '''
         fizzBuzzString = fizzbuzz(rawVal)
         resp.say(fizzBuzzString)
         return str(resp)
-        #return render_template('template.html',name=name)
     else:
         # If the dial fails:
         resp.say("The call failed, or the remote party hung up. Goodbye.")
@@ -123,30 +118,15 @@ def handle_key():
 '''
 frontend routes
 '''
-'''
-@app.route("/view")
-def my_form():
-    return render_template("my-form.html")
-
-@app.route("/view",methods = ['GET','POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    #if processed_text[:1] == "+1" and len(processed_text == 12):
-        #print("ayy lmao")
-    call(processed_text)
-    return my_form()
-'''
-
-@app.route("/views", methods=['GET', 'POST'])
-def outbound_call_initiate(call_failed=False):
+@app.route("/views/", methods=['GET', 'POST'])
+def callRoute(failedCall=False):
+  form = MyForm(request.form, failedCall=failedCall, csrf_enabled = False)
   resp = twilio.twiml.Response()
-  form = MyForm(request.form, call_failed=call_failed, csrf_enabled = False)
   if form.validate_on_submit():
-      return call(form.data['phone_num'])
+      return call(form.data['phoneNumber'])
   return render_template('form.html',
         title = 'Enter A Phone Number',
         form = form)
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
